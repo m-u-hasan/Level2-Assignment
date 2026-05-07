46318e3914e34e7aaaa957656cfbe7970f028383
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-07 15:43:02 +0600

    Solutions of Problem-3 & 3

--- solutions.ts
+++ solutions.ts
@@ -1,4 +1,4 @@
-//Prblem-1: create a filterEvenNumbers Function
+//Problem-1: create a filterEvenNumbers Function
 
 function filterEvenNumbers(numbers: number[]):number[]{
     return numbers.filter((num) => num%2==0);
@@ -7,10 +7,39 @@ function filterEvenNumbers(numbers: number[]):number[]{
 const result= filterEvenNumbers([1,2,3,4,5,6]);
 console.log(result);
 
-//Prblem-2: Reverse a String
+//Problem-2: Reverse a String
 
 function reverseString(str:String):string{
     return str.split("").reverse().join("");
 
 }
 console.log(reverseString("Hello"));
+
+
+//Problem-3: Check String or Number Data Type Using Union
+
+type StringOrNumber=string|number;
+function checkType(value: StringOrNumber): string{
+    if(typeof value=="string")
+    {
+        return "String";
+
+    }
+    return "Number";
+}
+console.log(checkType("Hello"));
+console.log(checkType(4555));
+
+//Problem-4: Generic function to return key value
+
+function getProperty<T, K extends keyof T> (obj: T, key: K): T[K]{
+    return obj[key];
+}
+
+const user={
+    id: 1,
+    name: "John Doe",
+    age: 21,
+};
+
+console.log(getProperty(user, "name"));
