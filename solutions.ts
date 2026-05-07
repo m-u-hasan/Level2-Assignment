1022552ed12f40940d1b2458e63f244bab6419ef
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-07 21:31:17 +0600

    Final Submission of Level 2 Assignment-1

--- solutions.ts
+++ solutions.ts
@@ -4,19 +4,12 @@ function filterEvenNumbers(numbers: number[]):number[]{
     return numbers.filter((num) => num%2==0);
 }
 
-const result= filterEvenNumbers([1,2,3,4,5,6]);
-console.log(result);
-
-
-
 //Problem-2: Reverse a String
 
 function reverseString(str:String):string{
     return str.split("").reverse().join("");
 
 }
-console.log(reverseString("Hello"));
-
 
 //Problem-3: Check String or Number Data Type Using Union
 
@@ -29,8 +22,6 @@ function checkType(value: StringOrNumber): string{
     }
     return "Number";
 }
-console.log(checkType("Hello"));
-console.log(checkType(4555));
 
 //Problem-4: Generic function to return key value
 
@@ -42,7 +33,6 @@ const user={
     name: "John Doe",
     age: 21,
 };
-console.log(getProperty(user, "name"));
 
 //Problem-5: a function accept a Book object and return a new object with an add isRead property
 
@@ -64,10 +54,9 @@ const myBook={
     author: "jane doe",
     publishedYear: 2004,
 };
-console.log(toggleReadStatus(myBook));
 
+//Problem-6: A Person Class & subClass Student
 
-//Problem-6:
 class Person{
     constructor(public name: string, public age: number){
 
@@ -86,9 +75,7 @@ class Student extends Person{
         return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
     }
 }
-const student =new Student ("Mahamud", 32, "A+");
- 
-console.log(student.getDetails());
+
 
 //Problem-7: Return a new arry containing only the elements that ar present in both array
 
@@ -96,5 +83,5 @@ function getIntersection(firstArray:number[], secondArray: number[]):number[]
 {
     return firstArray.filter((item)=>secondArray.includes(item));
 }
-console.log(getIntersection([1,2,3,4,5],[2,3,7,8]));
+
 
