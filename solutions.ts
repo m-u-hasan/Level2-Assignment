1ba0817d6c9cc9bf2a0efa6f378d27eb47791803
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-07 21:26:17 +0600

    Solutions of problem 1 to 7

--- solutions.ts
+++ solutions.ts
@@ -7,6 +7,8 @@ function filterEvenNumbers(numbers: number[]):number[]{
 const result= filterEvenNumbers([1,2,3,4,5,6]);
 console.log(result);
 
+
+
 //Problem-2: Reverse a String
 
 function reverseString(str:String):string{
@@ -42,6 +44,51 @@ const user={
 };
 console.log(getProperty(user, "name"));
 
+//Problem-5: a function accept a Book object and return a new object with an add isRead property
+
+interface Book{
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+function toggleReadStatus (book: Book):Book & {isRead: boolean}
+{
+    return{
+        ...book,
+        isRead:true,
+    };
+}
+
+const myBook={
+    title: "Typescript Guide",
+    author: "jane doe",
+    publishedYear: 2004,
+};
+console.log(toggleReadStatus(myBook));
+
+
+//Problem-6:
+class Person{
+    constructor(public name: string, public age: number){
+
+    }
+}
+class Student extends Person{
+    constructor(
+        name: string, 
+        age: number, 
+        public grade: string,
+    )
+        {
+        super (name, age);
+         }
+    getDetails():string{
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+const student =new Student ("Mahamud", 32, "A+");
+ 
+console.log(student.getDetails());
 
 //Problem-7: Return a new arry containing only the elements that ar present in both array
 
